333d49922d959485e4239694ed477ce230312cb7
Fmkhunt <fmkhunt018@gmail.com>
2025-12-12 19:58:20 +0530

    Update task service

--- hunt-service/src/services/task.service.ts
+++ hunt-service/src/services/task.service.ts
@@ -390,7 +390,7 @@ export class TaskService {
         )
       )
       .where(eq(huntTasksTable.hunt_id, huntId))
-      .groupBy(huntTasksTable.task_id);
+      .groupBy(tasksTable.id, completeTaskTable.id);
 
     return huntClaim;
     } catch (error) {
